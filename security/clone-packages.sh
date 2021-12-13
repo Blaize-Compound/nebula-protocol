c448cee8836840add7a9904b67a6626208a09b23
unknown <philipp.t@blaize.tech>
2021-12-13 10:47:24 +0200

    feat: prepare environment

---
+++ security/clone-packages.sh
@@ -0,0 +1,5 @@
+brownie pm install "OpenZeppelin/openzeppelin-contracts@4.2.0" || true
+brownie pm install "OpenZeppelin/openzeppelin-contracts-upgradeable@4.2.0" || true
+
+brownie pm clone "OpenZeppelin/openzeppelin-contracts@4.2.0" || true
+brownie pm clone "OpenZeppelin/openzeppelin-contracts-upgradeable@4.2.0" || true
